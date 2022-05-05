2411467799e41a671f5d05074ad557b459776a5b
ParasoI <ralpkendall@gmail.com>
2022-05-05 23:10:08 +0800

    -Add Menu Bar -Update MainPanel GridbagLayout -Add Fonts -Add Button Images

--- src/engine/main/MainFrame.java
+++ src/engine/main/MainFrame.java
@@ -1,11 +1,50 @@
 package engine.main;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 
 public class MainFrame extends JFrame {
 
+    private JMenuBar menuBar;
+    private JMenu fileMenu;
+    private JMenuItem createTask;
+    private JMenuItem openTask;
+    private JMenuItem deleteTask;
+
     public MainFrame(){
+
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+        createTask = new JMenuItem("Create Project");
+        openTask = new JMenuItem("Open Project");
+        deleteTask = new JMenuItem("Delete Project");
+
+        UIManager.put("PopupMenu.border", new LineBorder(Color.decode("#333333")));
+
+        menuBar.setBackground(Color.decode("#333333"));
+        menuBar.setBorderPainted(false);
+
+        fileMenu.setBackground(Color.decode("#333333"));
+        fileMenu.setForeground(Color.WHITE);
+        fileMenu.setBorderPainted(false);
+
+        createTask.setBackground(Color.decode("#333333"));
+        createTask.setForeground(Color.WHITE);
+        createTask.setBorderPainted(false);
+
+        openTask.setBackground(Color.decode("#333333"));
+        openTask.setForeground(Color.WHITE);
+        openTask.setBorderPainted(false);
+
+        deleteTask.setBackground(Color.decode("#333333"));
+        deleteTask.setForeground(Color.WHITE);
+        deleteTask.setBorderPainted(false);
+
+        fileMenu.add(createTask);
+        fileMenu.add(openTask);
+        fileMenu.add(deleteTask);
+        menuBar.add(fileMenu);
 
         this.setSize(500,300);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -14,6 +53,8 @@ public class MainFrame extends JFrame {
         this.setTitle("Project Scheduler");
         this.setResizable(false);
         this.setContentPane(new MainPanel());
+
+        this.setJMenuBar(menuBar);
 
         this.setVisible(true);
     }
--- src/engine/main/MainPanel.java
+++ src/engine/main/MainPanel.java
@@ -1,6 +1,7 @@
 package engine.main;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 
 public class MainPanel extends JPanel {
@@ -22,8 +23,32 @@ public class MainPanel extends JPanel {
         btn_reset = new JButton();
         btn_save = new JButton();
 
+        lbl_title.setForeground(Color.WHITE);
+        lbl_title.setFont(new Font("Bangers",Font.PLAIN,36));
+
+        lbl_taskName.setForeground(Color.WHITE);
+        lbl_taskName.setFont(new Font("Roboto",Font.PLAIN,14));
+
+        lbl_taskDuration.setForeground(Color.WHITE);
+        lbl_taskDuration.setFont(new Font("Roboto",Font.PLAIN,20));
+
+        btn_playPause.setPreferredSize(new Dimension(32,32));
+        btn_playPause.setBackground(null);
+        btn_playPause.setBorderPainted(false);
+        btn_playPause.setIcon(new ImageIcon(this.getClass().getResource("../../resources/play_32px.png")));
+
+        btn_reset.setPreferredSize(new Dimension(32,32));
+        btn_reset.setBackground(null);
+        btn_reset.setBorderPainted(false);
+        btn_reset.setIcon(new ImageIcon(this.getClass().getResource("../../resources/reset_32px.png")));
+
+        btn_save.setPreferredSize(new Dimension(32,32));
+        btn_save.setBackground(null);
+        btn_save.setBorderPainted(false);
+        btn_save.setIcon(new ImageIcon(this.getClass().getResource("../../resources/save_32px.png")));
+
         this.setPreferredSize(new Dimension(500,300));
-        this.setBackground(Color.BLUE);
+        this.setBackground(Color.decode("#0d0d0d"));
 
         GridBagLayout gridB = new GridBagLayout();
         this.setLayout(gridB);
@@ -31,32 +56,58 @@ public class MainPanel extends JPanel {
         GridBagConstraints gbc = new GridBagConstraints();
 
         gbc.fill = GridBagConstraints.CENTER;
+        gbc.weightx = 0.1;
+        gbc.weighty = 0.1;
+
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.gridwidth = 4;
+        gbc.gridwidth = 6;
+        gbc.gridheight = 2;
         gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(0,0,0,0);
         this.add(lbl_title,gbc);
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(10,0,0,0);
+        this.add(lbl_taskName,gbc);
+        gbc.gridwidth = 1;
+
+        gbc.gridx = 4;
+        gbc.gridy = 2;
+        gbc.gridwidth = 3;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(10,0,0,0);
+        this.add(lbl_taskDuration,gbc);
         gbc.gridwidth = 1;
 
         gbc.gridx = 0;
-        gbc.gridy = 1;
-        this.add(lbl_taskName,gbc);
-
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        this.add(lbl_taskDuration,gbc);
-
-        gbc.gridx = 1;
-        gbc.gridy = 2;
+        gbc.gridy = 3;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(0,0,20,0);
         this.add(btn_playPause,gbc);
+        gbc.gridwidth = 1;
 
         gbc.gridx = 2;
-        gbc.gridy = 2;
+        gbc.gridy = 3;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(0,0,20,0);
         this.add(btn_reset,gbc);
+        gbc.gridwidth = 1;
 
-        gbc.gridx = 3;
-        gbc.gridy = 2;
+        gbc.gridx = 4;
+        gbc.gridy = 3;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(0,0,20,0);
         this.add(btn_save,gbc);
+        gbc.gridwidth = 1;
 
     }
 }
